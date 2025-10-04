d06d8a6a02655a5b0da8e1c96e49901dbd86a1e2
gabriel cerdio <yo_mero_yo@hotmail.com>
2025-10-04 15:50:58 -0600

    1push

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+@type {import('tailwindcss').Config} 
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Aquí está la clave
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
